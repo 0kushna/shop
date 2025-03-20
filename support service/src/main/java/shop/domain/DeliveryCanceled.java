4259585299df1254a8acf1bc6b9a0058b7ba9350
0kushna <80585576+0kushna@users.noreply.github.com>
2025-03-20 15:04:51 +0900

    commit

---
+++ support service/src/main/java/shop/domain/DeliveryCanceled.java
@@ -0,0 +1,29 @@
+package shop.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import shop.domain.*;
+import shop.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryCanceled extends AbstractEvent {
+
+    private Long id;
+    private String productId;
+    private String quantity;
+    private String address;
+    private String orderId;
+    private String customerId;
+
+    public DeliveryCanceled(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryCanceled() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
